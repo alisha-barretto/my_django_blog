1f3e594f089881663f08cc6b7e7f838f2d4912e0
super-coder-girl <35896557+super-coder-girl@users.noreply.github.com>
2020-05-04 02:20:17 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['blog/templates','blog/templatetags'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

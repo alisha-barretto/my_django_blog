0cf3fbe9788edcfc3190e5aa6348da1040a384c1
Alisha Barretto <35896557+super-coder-girl@users.noreply.github.com>
2020-05-08 19:37:58 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,10 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog/static')]
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'blog/static/'),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog/static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -69,7 +68,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [STATICFILES_DIRS],
+        'DIRS': [STATIC_ROOT],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -134,7 +133,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 

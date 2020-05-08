8d3dfad20645c68c42ca318c1981ec3eb1803dbf
Alisha Barretto <35896557+super-coder-girl@users.noreply.github.com>
2020-05-08 19:33:22 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog/static')]
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'blog/static/'),
 ]
 
 
